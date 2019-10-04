5d228d902a9d1ddca8bb6b692f24a98acfff7411
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2019-10-04 17:24:36 -0400

    Disable CSRF protection

---
+++ src/main/java/example/bank/SecurityConfig.java
@@ -0,0 +1,23 @@
+package example.bank;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(requests ->
+                        requests.anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .csrf(csrf ->
+                        csrf.disable()
+                );
+    }
+}
